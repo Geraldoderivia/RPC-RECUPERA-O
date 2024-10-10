ed4ca9a35a8c54ce12e3d969bc2b9c811e43a65b
Geraldoderivia <brunoroters2310@gmail.com>
2024-10-10 08:39:18 -0300

    script commit 1
    criação das variáveis e início

--- Recuperação - Tanque/Assets/Scrpts/PlayerController.cs
+++ Recuperação - Tanque/Assets/Scrpts/PlayerController.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float moveSpeed = 5f;
+    public float rotateSpeed = 100f;
+    public GameObject projectilePrefab;
+    public Transform firePoint;
+    public int health = 100;
+
+    private int kills = 0;
+    private Vector3 spawnPosition;
+    // Método chamado no início do jogo
     void Start()
     {
-        
+        spawnPosition = transform.position;
     }
 
-    // Update is called once per frame
+    // Update é chamado uma vez por frame
     void Update()
     {
-        
+        HandleMovement();
+        HandleShooting();
     }
 }
